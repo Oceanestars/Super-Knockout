887dd00cad1157fe9b469c0a3162ccf6459f2b3f
Oceane Andreis <ocan3794@colorado.edu>
2022-03-06 00:44:07 -0700

    Working extension and minor style and readme changes
    - No longer using the service worker, you can just use chrome and query the title of the tab in our popup.js
    - Edited the data json file with more data
    - Edited the readme file for people to utilize the extension in developer mode
    - Styling of the popup contains a white background for higher contrast of list item. Also considered scenario with no data about the site.

--- TLDR/background.js
+++ TLDR/background.js
@@ -1,25 +1,8 @@
-function onError(error) {
-  console.error(`Error: ${error}`);
-}
-function sendMessageToTabs(tabs) {
-  chrome.tabs.sendMessage(
-    tabs.id,
-    {greeting: "Hi from background script"}
-  ).then(response => {
-    console.log("Message from the content script:" + tabs.title);
-    console.log(response.response);
-  }).catch(onError);
-}
+// chrome.scripting.executeScript (pass file)
+
 async function getCurrentTab() {
     let queryOptions = { active: true, currentWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
-    console.log(`Tab ${tab.id} is ${tab.url} and ${tab.title}`);
-    // sendMessageToTabs(tab)
-    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    //     chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-    //       console.log(response.farewell);
-    //     });
-    //   });
     return tab;
   }
 
